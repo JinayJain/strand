c533d70bb985c14e662aff0112e8adec98d28078
Jinay Jain <jinaybjain@gmail.com>
2023-07-15 22:55:37 -0400

    Remove new story creation UI

--- src/server/api/routers/story.ts
+++ src/server/api/routers/story.ts
@@ -1,43 +1,8 @@
 import { z } from "zod";
-import {
-  createTRPCRouter,
-  publicProcedure,
-  protectedProcedure,
-} from "@/server/api/trpc";
-import { v4 as uuidv4 } from "uuid";
+import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 import dayjs from "dayjs";
 
 export const storyRouter = createTRPCRouter({
-  createStory: protectedProcedure
-    .input(
-      z.object({
-        title: z.string(),
-        prompt: z.string(),
-        activeDate: z.date().optional(),
-      })
-    )
-    .mutation(async ({ input, ctx }) => {
-      const storyId = uuidv4();
-
-      return ctx.prisma.strandStory.create({
-        data: {
-          id: storyId,
-          title: input.title,
-          active_date: input.activeDate,
-          root: {
-            create: {
-              story_id: storyId,
-              content: input.prompt,
-              author_id: ctx.session.user.id,
-            },
-          },
-        },
-        include: {
-          root: true,
-        },
-      });
-    }),
-
   getCurrentStory: publicProcedure.query(async ({ ctx }) => {
     return ctx.prisma.strandStory.findFirst({
       where: {
