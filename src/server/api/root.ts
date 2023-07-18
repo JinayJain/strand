f46a0201a6ac10d725fe2175014d37ef4900762c
Jinay Jain <jinaybjain@gmail.com>
2023-07-18 00:05:56 -0400

    Add dayjs extend to backend

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -2,6 +2,12 @@ import { createTRPCRouter } from "@/server/api/trpc";
 import { strandRouter } from "./routers/strand";
 import { storyRouter } from "./routers/story";
 import { userRouter } from "./routers/user";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+dayjs.extend(utc);
+dayjs.extend(relativeTime);
 
 /**
  * This is the primary router for your server.
